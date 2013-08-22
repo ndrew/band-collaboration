10d0c0641d852afece1706cc66510592cc5a0675
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-08-23 00:04:21 +0300

    added pedestal server service

---
+++ server/project.clj
@@ -0,0 +1,29 @@
+(defproject server "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.pedestal/pedestal.service "0.2.0-SNAPSHOT"]
+
+                 ;; Remove this line and uncomment the next line to
+                 ;; use Tomcat instead of Jetty:
+                 
+                 ; in order to deploy to war jetty was commented
+                 ;
+                 [io.pedestal/pedestal.jetty "0.2.0-SNAPSHOT"]
+                 ;; [io.pedestal/pedestal.tomcat "0.2.0-SNAPSHOT"]
+
+                 ;; auto-reload changes
+                 [ns-tracker "0.2.1"]
+
+                 ;; Logging
+                 [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.2"]
+                 [org.slf4j/jcl-over-slf4j "1.7.2"]
+                 [org.slf4j/log4j-over-slf4j "1.7.2"]]
+  :profiles {:dev {:source-paths ["dev"]}}
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :aliases {"run-dev" ["trampoline" "run" "-m" "dev"]}
+  :main ^{:skip-aot true} server.server)
